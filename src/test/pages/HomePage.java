bbe83a871ac608df502e4942d9279a5c5722f35d
yaroslavLarionov <yarillo@yahoo.com>
2022-06-01 14:59:38 -0400

    created method test02 for covering US-02

--- src/test/pages/HomePage.java
+++ src/test/pages/HomePage.java
@@ -1,7 +1,9 @@
 package pages;
 
 import base.BasePage;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -15,7 +17,8 @@ public class HomePage extends BasePage {
 
     public String title = "National and Local Weather Radar, Daily Forecast, Hurricane and information from The Weather Channel and weather.com";
 
-
+    @FindBy (xpath = "//section[@id='privacy-data-notice']")
+    public WebElement alertMsg;
 
 
 }
--- src/test/tests/HomeTest.java
+++ src/test/tests/HomeTest.java
@@ -1,6 +1,13 @@
 package tests;
 
+import base.BasePage;
 import base.BaseTest;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -8,6 +15,7 @@ import pages.HomePage;
 
 public class HomeTest extends BaseTest {
     HomePage homePage;
+    BasePage basePage;
 
     @BeforeMethod
     public void localSetUp() {
@@ -17,6 +25,13 @@ public class HomeTest extends BaseTest {
     @Test (testName = "US-01", description = "Validate the title of the page")
     public void test01() {
         Assert.assertEquals(getDriver().getTitle(), homePage.title);
+    }
 
+    @Test (testName = "US-02", description = "Verify popup text")
+    public void test02() {
+        // Wait until pop-up window appears
+        homePage.waitForElementVisibility(homePage.alertMsg);
+        // Get the text of the pop-up window and close it
+        System.out.println(homePage.alertMsg.getText());
     }
 }
